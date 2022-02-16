1d943e0f435ccda0827f6df94734562c0f933cdc
Mihaela90 <mihaella2290@yahoo.com>
2022-02-16 18:39:31 +0100

    reparare carusel main page si filtru pagina toate produsele

--- caruselMainPagePractice.js
+++ caruselMainPagePractice.js
@@ -1,65 +1,61 @@
 let thumbnails = document.getElementsByClassName("thumbnail");
 
-			let activeImages = document.getElementsByClassName("active");
+let activeImages = document.getElementsByClassName("active");
 
-			for (var i = 0; i < thumbnails.length; i++) {
-				thumbnails[i].addEventListener("mouseover", function () {
-					console.log(activeImages);
+for (var i = 0; i < thumbnails.length; i++) {
+	thumbnails[i].addEventListener("mouseover", function () {
+		console.log(activeImages);
 
-					if (activeImages.length > 0) {
-						activeImages[0].classList.remove("active");
-					}
+		if (activeImages.length > 0) {
+			activeImages[0].classList.remove("active");
+		}
 
-					this.classList.add("active");
-					document.getElementById("featured").src = this.src;
-				});
-			}
+		this.classList.add("active");
+		document.getElementsByClassName("active").src =
+			this.src; /*aici in codul sursa de la carusel.js, foloseam document.getElementById("featured") insa in acest cod nu avem imaginea featured. Ceea ce vrem sa schimbam aici este clasa active, de la un thumbnail la altul, si nu clasa active de la featured la celelalte thumbnailuri.*/
+	});
+}
 
-			let buttonRight = document.getElementById("slideRight");
-			let buttonLeft = document.getElementById("slideLeft");
+let buttonRight = document.getElementById("slideRight");
+let buttonLeft = document.getElementById("slideLeft");
 
-			buttonLeft.addEventListener("click", function () {
-				document.getElementById("slider").scrollLeft -= 180;
-			});
+buttonLeft.addEventListener("click", function () {
+	document.getElementById("slider").scrollLeft -= 180;
+});
 
-			buttonRight.addEventListener("click", function () {
-				document.getElementById("slider").scrollLeft += 180;
-			});
+buttonRight.addEventListener("click", function () {
+	document.getElementById("slider").scrollLeft += 180;
+});
 
+let buttonRight1 = document.getElementById("slideRight1");
+let buttonLeft1 = document.getElementById("slideLeft1");
 
+buttonLeft1.addEventListener("click", function () {
+	document.getElementById("slider1").scrollLeft -= 180;
+});
 
-			let buttonRight = document.getElementById("slideRight1");
-			let buttonLeft = document.getElementById("slideLeft1");
+buttonRight1.addEventListener("click", function () {
+	document.getElementById("slider1").scrollLeft += 180;
+});
 
-			buttonLeft.addEventListener("click", function () {
-				document.getElementById("slider1").scrollLeft -= 180;
-			});
+let buttonRight2 = document.getElementById("slideRight2");
+let buttonLeft2 = document.getElementById("slideLeft2");
 
-			buttonRight.addEventListener("click", function () {
-				document.getElementById("slider1").scrollLeft += 180;
-			});
+buttonLeft2.addEventListener("click", function () {
+	document.getElementById("slider2").scrollLeft -= 180;
+});
 
+buttonRight2.addEventListener("click", function () {
+	document.getElementById("slider2").scrollLeft += 180;
+});
 
-			let buttonRight = document.getElementById("slideRight2");
-			let buttonLeft = document.getElementById("slideLeft2");
+let buttonRight3 = document.getElementById("slideRight3");
+let buttonLeft3 = document.getElementById("slideLeft3");
 
-			buttonLeft.addEventListener("click", function () {
-				document.getElementById("slider2").scrollLeft -= 180;
-			});
+buttonLeft3.addEventListener("click", function () {
+	document.getElementById("slider3").scrollLeft -= 180;
+});
 
-			buttonRight.addEventListener("click", function () {
-				document.getElementById("slider2").scrollLeft += 180;
-			});
-
-
-			let buttonRight = document.getElementById("slideRight3");
-			let buttonLeft = document.getElementById("slideLeft3");
-
-			buttonLeft.addEventListener("click", function () {
-				document.getElementById("slider3").scrollLeft -= 180;
-			});
-
-			buttonRight.addEventListener("click", function () {
-				document.getElementById("slider3").scrollLeft += 180;
-			});
-
+buttonRight3.addEventListener("click", function () {
+	document.getElementById("slider3").scrollLeft += 180;
+});
--- filtrareProduseNew.js
+++ filtrareProduseNew.js
@@ -5,11 +5,13 @@ for (i = 0; i < btns.length; i++) {
 	btns[i].addEventListener("click", (e) => {
 		e.preventDefault();
 
-		const filter = e.target.dataset.filter;
-		// console.log(filter)
+		/* with this, we target the data-filter from div class: filter-box*/
+		const filter = e.target.dataset.filter; 
+		/*console.log(filter); */
 		storeProducts.forEach((product) => {
 			if (filter == "all") {
-				product.style.display = "block";
+				/* if we click on the all filter, then.. */
+				product.style.display ="block"; 
 			} else {
 				if (product.classList.contains(filter)) {
 					product.style.display = "block";
@@ -21,23 +23,6 @@ for (i = 0; i < btns.length; i++) {
 	});
 }
 
-/*active class change of color */
-
-// Get the container element
-var btnContainer = document.getElementByClassName("filter-box");
-
-// Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("btn");
-
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
-	btns[i].addEventListener("click", function () {
-		var current = document.getElementsByClassName("active");
-		current[0].className = current[0].className.replace(" active", "");
-		this.className += " active";
-	});
-}
-
 /* Search filter */
 
 const search = document.getElementbyId("search");
@@ -45,22 +30,17 @@ const search = document.getElementbyId("search");
 search.addEventListener("keyup", (e) => {
 	e.preventDefault();
 	const searchValue = search.value.toLowerCase().trim();
-	const allProducts = document.querySelectorAll("#store-products");
+	/*const allProducts = document.querySelectorAll("#store-products"); */
 
-	for (i = 0; i < allProducts.length; i++) {
-		if (allProducts[i].classList.contains(searchValue)) {
-			allProducts[i].style.display == "block";
+	for (i = 0; i < storeProducts.length; i++) {
+		if (storeProducts[i].classList.contains(searchValue)) {
+			storeProducts[i].style.display = "block";
 		} else if (searchValue == "") {
-			allProducts[i].style.display == "block";
+			storeProducts[i].style.display = "block";
 		} else {
-			allProducts[i].style.display == "none";
+			storeProducts[i].style.display = "none";
 		}
 	}
 });
 
-/*var allProducts = document.querySelectorAll("#store-products");
 
-[15:42, 15/02/2022] Marius Catalin POPUSOI: pai trebuie luate elementele cu acea clasa
-[15:43, 15/02/2022] Marius Catalin POPUSOI: deci trebuie sa folosesti un querySelectorAll
-[15:43, 15/02/2022] Marius Catalin POPUSOI: sa stochezi intr-o variabila
-[15:43, 15/02/2022] Marius Catalin POPUSOI: si acea variabila sa fie folosita in for */
